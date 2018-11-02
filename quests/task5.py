6d787dcd028f89edae448465a60f5ab901f9929f
zahessi <rievva.ara@gmail.com>
2018-11-02 17:34:13 +0200

    доделала пятую таску, идею случайно прочитала и уже реализовала

--- quests/task5.py
+++ quests/task5.py
@@ -1,10 +1,19 @@
 # Написать функцию, ктр будет проверять можно ли преобразовать строку так,
 # чтобы она стала палиндромом.
 
-
 def just_palindrom(s):
     s = ''.join(filter(lambda x: x if str.isalnum(x) else None, s))
     return s == s[::-1]
 
-assert just_palindrom("101") == True
-assert just_palindrom('а роза упала на лапу азора') == True
+def possible_palindrom(s):
+    one = False
+    s = [x for x in sorted(s) if x.isalnum()]
+    for i in s:
+        if s.count(i) == 1 and not one: 
+            one = True
+        elif s.count(i) % 2 and one:
+            return False
+    return True
+
+assert possible_palindrom("саша") == True
+assert possible_palindrom('а роза упала на лапу азора') == True
