f5f51073245120e7978d565445ef71c41c354bbd
zahessi <rievva.ara@gmail.com>
2018-11-12 20:10:33 +0200

    upgr

--- quests/task10.py
+++ quests/task10.py
@@ -1,13 +1,10 @@
 # Если элемент матрицы равен 0, то всю строку и весь столбец нужно обнулить.
 def zero(arr):
-    zero_count = arr.count(0)
     zeroes_pointers = []
     for i, m in enumerate(arr):         # строка
         for j, el in enumerate(m):      # столбец
             if el == 0: 
                 zeroes_pointers.append((i, j))
-                zero_count -= 1
-        if not zero_count: break
 
     for i, j in zeroes_pointers:
         for ind, el in enumerate(arr[i]): arr[i][ind] = 0
@@ -15,5 +12,4 @@ def zero(arr):
     return arr
 
     
-for x in zero([[0, 2, 3], [4, 0, 6], [7, 8, 0]]):
-    print(*x, sep=" ")
+assert zero([[1, 2, 3], [4, 5, 6], [7, 8, 0]]) == [[1, 2, 0],[4, 5, 0],[0, 0, 0]]
